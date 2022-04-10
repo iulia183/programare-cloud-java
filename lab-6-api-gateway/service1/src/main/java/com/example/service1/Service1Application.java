35d0b03eb38edfc6d539906fac377121b67c1a42
iulia183 <38918954+iulia183@users.noreply.github.com>
2022-04-10 21:42:23 +0300

    Service1Application try

--- lab-6-api-gateway/service1/src/main/java/com/example/service1/Service1Application.java
+++ lab-6-api-gateway/service1/src/main/java/com/example/service1/Service1Application.java
@@ -2,11 +2,17 @@ package com.example.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 public class Service1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Service1Application.class, args);
 	}
 
@@ -16,5 +22,35 @@ public class Service1Application {
 	// 3. print request headers
 	// 4. register the service in eureka
 
+	public class Greeting {
+
+		private final long id;
+		private final String content;
+
+		public Greeting(long id, String content) {
+			this.id = id;
+			this.content = content;
+		}
+
+		public long getId() {
+			return id;
+		}
+
+		public String getContent() {
+			return content;
+		}
+	}
+
+	@RestController
+	public class GreetingController {
+
+		private static final String template = "Hello, %s!";
+		private final AtomicLong counter = new AtomicLong();
+
+		@GetMapping("/greeting")
+		public Greeting greeting(@RequestParam(value = "name", defaultValue = "Brasov") String name) {
+			return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		}
+	}
 
 }
